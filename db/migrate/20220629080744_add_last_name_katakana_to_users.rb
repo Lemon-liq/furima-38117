8a96e07222f57b6a085887ed5e419546fa68a2db
Lemon-liq <fujita_2022@yahoo.co.jp>
2022-06-30 21:47:15 +0900

    新規登録のバリデーション

--- db/migrate/20220629080744_add_last_name_katakana_to_users.rb
+++ db/migrate/20220629080744_add_last_name_katakana_to_users.rb
@@ -1,5 +1,5 @@
 class AddLastNameKatakanaToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :last_name_katakana, :string
+    add_column :users, :last_name_katakana, :string, null: false
   end
 end
