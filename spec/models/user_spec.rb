cf84fdcad37ae8567897d0085db6bbb2216874b9
Lemon-liq <fujita_2022@yahoo.co.jp>
2022-07-03 01:05:04 +0900

    修正1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordが129文字以上では登録できない' do
-        @user.password = Faker::Internet.password(min_length: 129)
+        @user.password =  Faker::Lorem.characters(number: 129, min_alpha: 1, min_numeric: 1) 
         @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
