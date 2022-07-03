e160354d30e06fbe682558e4943de7babe8fac79
Lemon-liq <fujita_2022@yahoo.co.jp>
2022-07-03 20:57:07 +0900

    出品画面に遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:price, :text, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :number_id, :user)
   end
 end
